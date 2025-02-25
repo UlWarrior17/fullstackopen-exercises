e4f66ecf9cdd9437040729634c713edb0d88d7d4
UlWarrior17 <ulrichneud@gmail.com>
2025-02-25 16:33:27 +0100

    step 5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,7 @@
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
+
 const Part = (props) => {
   return (
     <div>
@@ -10,6 +11,7 @@ const Part = (props) => {
     </div>
   );
 };
+
 const Content = (props) => {
   return (
     <div>
@@ -21,7 +23,6 @@ const Content = (props) => {
 };
 
 const Total = (props) => {
-  console.log(props);
   const sum =
     props.parts[0].exercises +
     props.parts[1].exercises +
@@ -32,28 +33,31 @@ const Total = (props) => {
     </div>
   );
 };
+
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
